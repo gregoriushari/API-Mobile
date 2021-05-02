1633dd336b33b29259b6ca37b1a0187b438612de
Ghs267 <hariyantosetiadi@gmail.com>
2021-05-03 00:36:30 +0700

    edit signup

--- app/middleware/verifySignUp.js
+++ app/middleware/verifySignUp.js
@@ -1,18 +1,7 @@
 const db = require("../models")
 const User = db.user
 
-checkDuplicateUsernameOrEmail = (req, res, next)=>{
-    User.findOne({
-        where:{
-            username: req.body.username
-        }
-    }).then(user=>{
-        if(user){
-            res.status(400).send({
-                message: "Failed! Username is already in use!"
-            })
-            return
-        }
+checkEmail = (req, res, next)=>{
         //check email
         User.findOne({
             where: {
@@ -27,11 +16,10 @@ checkDuplicateUsernameOrEmail = (req, res, next)=>{
             }
             next()
         })
-    })
 }
 
 const verifySignUp = {
-    checkDuplicateUsernameOrEmail: checkDuplicateUsernameOrEmail
+    checkEmail: checkEmail
 }
 
 module.exports = verifySignUp
